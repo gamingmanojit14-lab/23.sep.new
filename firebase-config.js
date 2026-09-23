828e3a3824483a7861bcb743168f44218c5d6eea
gamingmanojit14-lab <gamingmanojit14@gmail.com>
2026-09-23 23:16:35 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+/* ============================================================
+   🔥 FIREBASE CONFIGURATION
+   Project: fir-c6c4c
+   ============================================================ */
+
+/*const FIREBASE_CONFIG = {
+  apiKey:            "AIzaSyDc8Ui4b7oXfjpngns6Vd7TFyCfkt1TJeQ",
+  authDomain:        "fir-c6c4c.firebaseapp.com",
+  projectId:         "fir-c6c4c",
+  storageBucket:     "fir-c6c4c.firebasestorage.app",
+  messagingSenderId: "977248213751",
+  appId:             "1:977248213751:web:09cea129b8a72e1ca011f2"
+};
+
+window.FIREBASE_CONFIG = FIREBASE_CONFIG;*/
